b18e11a86858c844a7cd3f3d43cca3c35354f3fd
LuSamaniego <62125618+LucasSamaniego@users.noreply.github.com>
2025-05-28 00:34:37 -0300

    Add files via upload
    Database to query.

---
+++ DataBase/colocar_dados_tabela.sql
@@ -0,0 +1,8 @@
+INSERT INTO item (id, nomenclatura, pn, sn)
+VALUES 
+    (1, 'Parafuso', 'PN12345', 'SN54321'),
+    (2, 'Porca', 'PN67890', 'SN09876'),
+    (3, 'Arruela', 'PN11223', 'SN33211');
+
+
+select * from item;
