1872bd0524b9905de0eee2435add1fe7ce0f396c
LuSamaniego <62125618+LucasSamaniego@users.noreply.github.com>
2025-06-04 20:07:40 -0300

    Update draw.py

--- Raspberry Pi 4B/Display OLED/draw.py
+++ Raspberry Pi 4B/Display OLED/draw.py
@@ -4,6 +4,6 @@ import time
 disp, image, draw = init_display()
 
 directions = ["frente", "tras", "esquerda", "direita"]
-for d in directions:
-    draw_triangle_on_display(disp, draw, image, d)
-    time.sleep(3)
+dir = "frente"
+draw_triangle_on_display(disp, draw, image, dir)
+time.sleep(3)
